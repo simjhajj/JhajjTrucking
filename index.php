7aac9893c737a9c22936b870c4a652513e632c93
Simrandeep <200554998@studebt.georgianc.on.ca>
2023-11-28 21:26:34 -0500

    <modified index.html>

--- index.php
+++ index.php
@@ -9,14 +9,9 @@
 </head>
 <body>
     <!-- Page header -->
-    <header>
-        <h1>Jhajj Transport</h1>
-        <!-- Navigation Bar -->
-        <ul class="navigation">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="view.php">View</a></li>
-        </ul>
-    </header>
+    <? 
+        include("header.php");
+    ?>
     <!-- Page Main -->
     <main>
         <div class="container">
@@ -75,50 +70,8 @@
             </div>
     </main>
     <!-- Page Footer -->
-    <footer>
-        <section>
-            <div>
-                <p class="conclusion">We invite you to join us on this journey. Whether you are a client, partner, or prospective team member, Jhajj Transport is here to provide you with the 
-                    best in transportation and logistics services. Together, we'll drive success.Thank you for considering Jhajj Transport for your transportation needs.
-                    We look forward to serving you and building a brighter future together.</p>
-                   
-            </div>
-            <!-- Footer Links -->
-            <div class="impLinks">
-            <div class="links1">
-                <!-- List 1 of links -->
-                <ul class="l1">
-                    <li><h3>About</h3></li>   
-                    <li><a href="www.google.com">Join Jhajj Transport</a></li>
-                    <li><a href="www.google.com">Our locations</a></li>
-                    <li><a href="www.google.com">CRS Initiatives</a></li>
-                    <li><a href="www.google.com">Corporate Membership</a></li>    
-                </ul>
-            </div>
-            <div class="links2">
-                <!-- List 2 of links -->
-                <ul class="l1">
-                    <li><h3>Contact us:</h3></li>   
-                    <li><a href="www.google.com">Email</a></li>
-                    <li><a href="www.google.com">Locations</a></li>
-                    <li><a href="www.google.com">Delivery Schedules</a></li>    
-                </ul>
-            </div>
-            <!-- List 3 of links -->
-             <div class="sclinks">
-                    <h3>Follow us on: </h3>
-                    <i class="fab fa fa-linkedin"></i>
-                    <i class="fab fa fa-twitter"></i>
-                    <i class="fab fa fa-instagram"></i>
-                    <i class="fab fa fa-youtube"></i>
-                    <i class="fab fa fa-facebook-f"></i>
-            </div>
-            </div>
-            <!-- Copyright Message by Company -->
-            <div class="copyright">
-                 <p>&copy 2023 Jhajj Transport, Inc. All rights reserved</p>        
-            </div>
-        </section>
-    </footer>
+    <?php
+       include("footer.php");
+    ?>
 </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- link for connecting to styles.css -->
+    <script src="https://kit.fontawesome.com/457a315592.js"crossorigin="anonymous"></script> <!-- link for connecting to icons-->
+    <title>Jhajj Transport</title>
+</head>
+<body>
+    <!-- Page header -->
+    <header>
+        <h1>Jhajj Transport</h1>
+        <!-- Navigation Bar -->
+        <ul class="navigation">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="view.php">View</a></li>
+        </ul>
+    </header>
